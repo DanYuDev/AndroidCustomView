bf747688fed13b5cab74c200814be8b0913ee704
coderlt <coderliantang@gmail.com>
2018-02-04 21:21:48 +0800

    Simple CircleImageView extends ImageView

---
+++ CircleImageView/BitmapShaderTest/app/src/main/java/com/example/coderlt/bitmapshadertest/view/CircleImageView.java
@@ -0,0 +1,80 @@
+package com.example.coderlt.bitmapshadertest.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * -----------------------------------------
+ * CircleImageView
+ * 继承自 ImageView，没有添加另外的属性
+ * 对宽高测量做了简单处理，保证是一个正方形的View
+ * ----------------------------------------
+ * 利用 BitmapShader 渲染一个圆形，很容易得到这个 CircleImageView
+ * 主要测试下继承 CircleImageView 可以利用哪些属性
+ * Created by coderlt on 2018/2/4.
+ */
+
+public class CircleImageView extends ImageView {
+    private static final String TAG = "CircleImageView";
+    private int mWidth,mHeight;
+    private Paint mPaint;
+    private Bitmap bitmap;
+    private float cx,cy,radius;
+
+    public CircleImageView(Context context){
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs){
+        super(context,attrs);
+        init();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec,int heightMeasureSpec){
+        super.onMeasure(widthMeasureSpec,heightMeasureSpec);
+        mWidth = getMeasuredWidth();
+        mHeight = getMeasuredHeight();
+
+        radius = Math.min(mWidth,mHeight);
+        // 得到最适合圆角图片的正方形
+        setMeasuredDimension((int)radius,(int)radius);
+    }
+
+    /**
+     * 调用完父构造函数（super(context,attrs)）之后，就可以得到 Drawable 对象
+     */
+    private void init(){
+        // --------------------key  codes----------------------------------
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) getDrawable();
+        bitmap = bitmapDrawable.getBitmap();
+        //--------------------------------------------------------------
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        Shader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+        mPaint.setShader(shader);
+    }
+
+    @Override
+    protected void onSizeChanged(int x,int y,int oldx,int oldy){
+        mWidth = x;
+        mHeight = y;
+
+        cx = x/2;
+        cy = y/2;
+        radius = Math.min(cx,cy);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+
+        canvas.drawCircle(cx,cy,radius,mPaint);
+    }
+
+}
